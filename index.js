90407b54789557a39dc42d7fc2790e1702fd2979
demirhancosku <cosku@dokuziki.com>
2017-12-24 23:47:16 +0300

    polo to trex

--- index.js
+++ index.js
@@ -1,60 +1,101 @@
 const Twit = require('twit');
 const names = require('./names.js');
-const Poloniex = require('./poloniex');
+const Bittrex = require('node-bittrex-api');
 
 const API_KEY = '';
 const SECRET = '';
 
-var IDChecked = [];
-const safeCheck = true; //HIGHLY recomended~
+let IDChecked = [];
+let multip = 1.25; // This value will be multiply with last ask price for buy order
+let BTCAmount = 0.1;
 
-/* See Documentation on poloniex.js */
-const polo = new Poloniex(API_KEY, SECRET, 0, 0);
-
-const T = new Twit({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
-  access_token_secret: ''
+Bittrex.options({
+    'apikey': API_KEY,
+    'apisecret': SECRET,
 });
 
-var stream = T.stream('statuses/filter', { follow : '961445378' });
+const T = new Twit({
+    consumer_key: 'EtOiey9DWfAeGBSAdARVnQ',
+    consumer_secret: 'wy9SUMOEeFKeRYn5R9PiF0p2Ir5Oz8uplGnI045e9o',
+    access_token: '176911783-seArpdcBUDJ3RD2ysOtc39OOTLO39LOXcDZzcu1i',
+    access_token_secret: 'sNHJGbEek0x8pfGCvUgNM6ZR66yFsvHDVg9ICLab7N0Sk'
+});
+
+let stream = T.stream('statuses/filter', {follow: '176911783' /*'961445378' */});
 
 stream.on('tweet', (tweet, err) => {
     if (!isIn(tweet.id)) {
-      checkTweet(tweet.text);
+        checkTweet(tweet.text);
     } else {
-      console.log(`Found tweet ${tweet.id} but it has already been processed`);
+        console.log(`Found tweet ${tweet.id} but it has already been processed`);
     }
 })
 
-function checkTweet(text){
-  text = text.toLowerCase();
-  if (safeCheck) {
-    for (var val of names) {
-      if (text.includes(val.toLowerCase()) && text.toLowerCase().includes('coin of the day')) {
-        console.log(`${text} :: ${val}`);
-        polo.checkBalancesandBuy(val.toUpperCase());
-      }
+function checkTweet(text) {
+    text = text.toLowerCase();
+
+    for (let val of names) {
+        if (text.includes("("+val.toLowerCase()+")") && text.toLowerCase().includes('coin of the day')) {
+
+            console.log(`${text} :: ${val}`);
+            /**
+             * Check Price
+             */
+            Bittrex.getticker( { market : 'BTC-'+val.toUpperCase() }, function( ticker ) {
+
+                let ask = ticker.result.Ask  * multip;
+
+                let quantity = parseInt(BTCAmount / ask)-1;
+                /**
+                 * Buy
+                 */
+                Bittrex.tradebuy({
+                    MarketName: 'BTC-'+val.toUpperCase(),
+                    OrderType: 'LIMIT',
+                    Quantity: quantity,
+                    Rate: ask,
+                    TimeInEffect: 'GOOD_TIL_CANCELLED',
+                    ConditionType: 'NONE',
+                    Target: 0,
+                }, function( data, err ) {
+
+                    console.log( data );
+                    console.log( err );
+
+                    let realAmount = data.bişi
+
+                    Bittrex.tradesell({
+                        MarketName: 'BTC-'+val.toUpperCase(),
+                        OrderType: 'LIMIT',
+                        Quantity: 1.00000000,
+                        Rate: ask * multip * 2.5,
+                        TimeInEffect: 'GOOD_TIL_CANCELLED',
+                        ConditionType: 'LESS_THAN',
+                        Target: 0,
+                    }, function( data, err ) {
+                        console.log( data );
+                        console.log( err );
+                    });
+
+                });
+
+            });
+
+
+
+        }
     }
-  } else {
-    for (var val of names) {
-      if (text.includes(val)) {
-        console.log(`${text} :: ${val}`);
-        polo.checkBalancesandBuy(val.toUpperCase());
-      }
-    }
-  }
+
 }
 
 function isIn(id) {
-  for (var val of IDChecked) {
-    if (val === id) {
-      return true;
+    for (var val of IDChecked) {
+        if (val === id) {
+            return true;
+        }
     }
-  }
-  IDChecked.push(id);
-  return false;
+    IDChecked.push(id);
+    return false;
 }
 
 
